cd250a7237dbbd619f87563d0ea983084878ce58
James Higgins <jhiggins0329@gmail.com>
2016-10-27 15:09:21 -0400

    Add files via upload

--- palindrome.py
+++ palindrome.py
@@ -1,12 +1,16 @@
-def is_palindrome(sentence):
-    # TODO: return True or False if the sentence is or isn't a palindrome
-    pass
+def is_palindrome(user_input):
+        palindrome = string_cleaner(user_input)
+        return palindrome == palindrome[::-1]
+
+def output():
+    verdict = ""
+    if not is_palindrome(user_input):
+        verdict = "not "
+    print(user_input, "is {}a palindrome".format(verdict))
 
 
 def main():
-    # TODO: put your input/output code here
-    pass
-
+    user_input = input("enter a suspected palindrome")
 
 if __name__ == '__main__':
     main()
